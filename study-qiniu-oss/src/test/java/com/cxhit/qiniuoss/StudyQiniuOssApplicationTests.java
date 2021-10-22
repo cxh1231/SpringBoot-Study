12fa77d2b956d62674dac0df09ddf949813a263a
cxh1231 <363538880@qq.com>
2021-10-22 12:01:10 +0800

    初始化七牛云OSS组件.

---
+++ study-qiniu-oss/src/test/java/com/cxhit/qiniuoss/StudyQiniuOssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cxhit.qiniuoss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyQiniuOssApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
