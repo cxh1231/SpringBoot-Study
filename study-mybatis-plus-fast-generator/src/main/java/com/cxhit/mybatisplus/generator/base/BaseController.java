c1d8c91a43a87254e29baa79a90403c34f65de9a
cxh1231 <363538880@qq.com>
2021-10-26 20:35:48 +0800

    :white_check_mark: Mybatis-Plus-Generator(3.5.1+)代码生成器组件集成.

---
+++ study-mybatis-plus-fast-generator/src/main/java/com/cxhit/mybatisplus/generator/base/BaseController.java
@@ -0,0 +1,12 @@
+package com.cxhit.mybatisplus.generator.base;
+
+/**
+ * <p>
+ * 这是测试用的，控制类的父类
+ * </p>
+ *
+ * @author 拾年之璐
+ * @since 2021-10-25 0025 19:47
+ */
+public class BaseController {
+}
---
+++ study-mybatis-plus-fast-generator/src/main/java/com/cxhit/mybatisplus/generator/base/BaseEntity.java
@@ -0,0 +1,12 @@
+package com.cxhit.mybatisplus.generator.base;
+
+/**
+ * <p>
+ * 这是测试用的，实体的父类
+ * </p>
+ *
+ * @author 拾年之璐
+ * @since 2021-10-25 0025 19:46
+ */
+public class BaseEntity {
+}
