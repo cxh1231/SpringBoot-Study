79381c71e0d67d4068f6791f954fa33d3f5a1952
cxh1231 <363538880@qq.com>
2021-11-12 21:42:53 +0800

    :tada: 初始化七牛云OSS项目.

---
+++ study-oss-qiniu/src/main/java/com/cxhit/oss/qiniu/StudyOssQiniuApplication.java
@@ -0,0 +1,18 @@
+package com.cxhit.oss.qiniu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 项目启动入口
+ *
+ * @author 拾年之璐
+ * @since 2021/11/12 21:36
+ */
+@SpringBootApplication
+public class StudyOssQiniuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyOssQiniuApplication.class, args);
+    }
+}
