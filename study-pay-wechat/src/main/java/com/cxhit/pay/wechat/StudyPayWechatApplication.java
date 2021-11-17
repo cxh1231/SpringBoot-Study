79df05146fbe9ae3aa40f67719827d59195ad445
cxh1231 <363538880@qq.com>
2021-11-17 21:08:01 +0800

    :tada: 初始化微信支付组件.

---
+++ study-pay-wechat/src/main/java/com/cxhit/pay/wechat/StudyPayWechatApplication.java
@@ -0,0 +1,20 @@
+package com.cxhit.pay.wechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ * 描述
+ * </p>
+ *
+ * @author 拾年之璐
+ * @since 2021/11/17 20:09
+ */
+@SpringBootApplication
+public class StudyPayWechatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyPayWechatApplication.class, args);
+    }
+}
---
+++ study-pay-wechat/src/test/java/com/cxhit/pay/wechat/StudyPayWechatApplicationTests.java
@@ -0,0 +1,15 @@
+package com.cxhit.pay.wechat;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * <p>
+ * 描述
+ * </p>
+ *
+ * @author 拾年之璐
+ * @since 2021/11/17 20:12
+ */
+@SpringBootTest
+public class StudyPayWechatApplicationTests {
+}
