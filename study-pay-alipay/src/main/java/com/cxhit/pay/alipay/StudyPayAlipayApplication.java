bd24413bd08ee945340d094fc840707cbdd75591
cxh1231 <363538880@qq.com>
2021-11-17 21:07:33 +0800

    :tada: 初始化支付宝支付组件.

---
+++ study-pay-alipay/src/main/java/com/cxhit/pay/alipay/StudyPayAlipayApplication.java
@@ -0,0 +1,20 @@
+package com.cxhit.pay.alipay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ * 描述
+ * </p>
+ *
+ * @author 拾年之璐
+ * @since 2021/11/17 20:06
+ */
+@SpringBootApplication
+public class StudyPayAlipayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyPayAlipayApplication.class, args);
+    }
+}
