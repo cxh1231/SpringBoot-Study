5f25ac40befb3f13eef4a761a6b2031493f08046
cxh1231 <363538880@qq.com>
2021-11-17 19:55:04 +0800

    :tada: 初始化阿里云OSS对象存储服务组件.

---
+++ study-oss-aliyun/src/main/java/com/cxhit/oss/aliyun/StudyOssAliyunApplication.java
@@ -0,0 +1,20 @@
+package com.cxhit.oss.aliyun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ * 描述
+ * </p>
+ *
+ * @author 拾年之璐
+ * @since 2021/11/17 19:45
+ */
+@SpringBootApplication
+public class StudyOssAliyunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyOssAliyunApplication.class, args);
+    }
+}
---
+++ study-oss-aliyun/src/test/java/com/cxhit/oss/aliyun/StudyOssAliyunApplicationTests.java
@@ -0,0 +1,15 @@
+package com.cxhit.oss.aliyun;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * <p>
+ * 描述
+ * </p>
+ *
+ * @author 拾年之璐
+ * @since 2021/11/17 19:47
+ */
+@SpringBootTest
+public class StudyOssAliyunApplicationTests {
+}
